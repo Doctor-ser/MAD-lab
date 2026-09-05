e48737114af09f5159b4d9b4192f5265ff22a02e
Doctor-ser <112398684+Doctor-ser@users.noreply.github.com>
2026-09-05 15:32:29 +0530

    Add files via upload

---
+++ optionmenu13/ActivityOne.java
@@ -0,0 +1,14 @@
+package com.example.optionmenu13;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityone);
+    }
+}
