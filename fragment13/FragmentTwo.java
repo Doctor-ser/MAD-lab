6684be1c7c0e84ac1495621a6106630ccb85fc62
Doctor-ser <112398684+Doctor-ser@users.noreply.github.com>
2026-09-08 14:44:28 +0530

    Add files via upload

---
+++ fragment13/FragmentTwo.java
@@ -0,0 +1,24 @@
+package com.example.fragment13;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+// This is the second fragment class.
+public class FragmentTwo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment.
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
